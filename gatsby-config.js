8f1305ae30c2f6c38502b9f3b4be069bb94b2105
Edwards <ASEdwards@SCIRES.LOCAL>
2021-06-23 17:59:03 -0400

    Changed Site name

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 module.exports = {
   siteMetadata: {
-    title: `Creative Portfolio`,
+    title: `Metadata Search`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
